0cfdfe8627f52ea2759347b8e61fea7138468a27
Debora Oktaria Lubis <deboraoktarialubis@gmail.com>
2023-01-20 14:52:35 +0700

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const usersRoutes = require('./src/users/routes');
 const app = express();
 const port = 3000;
 
-app.get("/", (req,res) => {
-    res.send ("Hello World!");
-});
+app.use(express.json());
 
-app.listen(port, () => console.log('app listenig on port ${port}'));
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+app.use("/api/v1/users", usersRoutes);
+
+app.listen(port, () => console.log('app listening on port 3000'));
---
+++ src/users/routes.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const controller = require('./controller')
+
+const router = Router();
+
+router.get("/", controller.getUsers);
+router.post("/", controller.addUsers);
+router.get("/:id", controller.getUsersById);
+router.delete("/:id", controller.removeUsers);
+router.put("/:id", controller.updateUsers);
+
+module.exports = router;
